4f57a430964be0e6d9f87ae38f99f7b6d69ef4f7
TekkertheChaot <jjmn@live.de>
2017-08-29 09:19:44 +0200

    Added readline to prevent disappearing

--- ConsoleApplication1/ConsoleApplication3/Program.cs
+++ ConsoleApplication1/ConsoleApplication3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ namespace ConsoleApplication3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello second Project!");
+            Cosnole.ReadLine();
         }
     }
 }

5fb132c0c88a397f50bff19c7aebbc6a196b2b66
Jannik <jjmn@live.de>
2017-08-29 09:21:30 +0200

    edited typo

--- ConsoleApplication1/ConsoleApplication3/Program.cs
+++ ConsoleApplication1/ConsoleApplication3/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,7 @@ namespace ConsoleApplication3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello second Project!");
-            Cosnole.ReadLine();
+            Console.ReadLine();
         }
     }
 }

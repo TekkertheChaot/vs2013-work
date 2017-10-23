7712e841ee2166bcbcf99082fda09a5d6e3f4cb2
Jannik Neumann <jjmn@live.de>
2017-10-23 15:25:48 +0200

    created

---
+++ Operatoren-Exercice/Operatoren/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatoren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = x++;
+            Console.Write(x);
+            Console.Write(y);
+            Console.Write(x--);
+            Console.Write(x);
+            Console.ReadLine();
+        }
+    }
+}

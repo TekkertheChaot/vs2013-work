704e6c8c0736a259ce4d96bccdf354f6f2be4ec9
Jannik Neumann <jjmn@live.de>
2017-10-23 15:09:46 +0200

    Created "Visitenkarten" execice

---
+++ ConsoleApplication1/Visitenkarte/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitenkarte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\n\n\n\n");
+            Console.WriteLine("\t\t\t\t|*************************|");
+            Console.WriteLine("\t\t\t\t|* Jannik Neumann        *|");
+            Console.WriteLine("\t\t\t\t|* Zentgrafenstraße 49   *|");
+            Console.WriteLine("\t\t\t\t|* 60389 Frankfurt a. M. *|");
+            Console.WriteLine("\t\t\t\t|* Geb. 24.11.1998 | BRD *|");
+            Console.WriteLine("\t\t\t\t|*************************|");
+            Console.WriteLine("\n\n\n\n\n\n");
+            Console.ReadLine();
+        }
+    }
+}
